4354cc896e23a33e8a12ee652132185a6de1bcd3
Vladyslav <prostoandrei9@gmail.com>
2025-01-30 00:28:31 +0200

    feat: clickable links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,59 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import styles from "./page.module.css";
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [text, setText] = useState("");
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const fullText = `> Welcome to my web developer portfolio
+> Name: Vladyslav Kovalchuk
+> Main stack: Typescript, React, Next, Shadcn, React-Query
+> Last Projects:
+  1. Shoe Shop
+  2. Blockchain Chat Application
+  3. TBA
+> Contact: 
+  Email - prostoandrei9@gmail.com
+  Telegram - <a target="_blank" href="https://t.me/vlad_kvlchk" className="link">t.me/vlad_kvlchk</a>
+  Github: <a target="_blank" href="https://github.com/vladkvlchk" className="link">github.com/vladkvlchk</a>
+
+> About me: I'm desired with web development. 
+  Mostly I like front-end part, but I also can implement backend or smartcontracts. 
+  I expect to find a job opportunity to work with a team of professionals that will positively impact on my growing-up in Saas or/and Web3 development areas. 
+
+Type 'help' for available commands.
+`;
+
+  useEffect(() => {
+    let i = 0;
+    const typing = setInterval(() => {
+      setText(fullText.slice(0, i));
+      i++;
+      if (i > fullText.length) {
+        clearInterval(typing);
+      }
+    }, 10);
+
+    return () => clearInterval(typing);
+  }, []);
+
+  return (
+    <main className={styles.console}>
+      <div className="">
+        <div className="h-max float-left mr-4 mb-4">
+          <div className="bg-black h-3 w-14 translate-y-[70px] mx-auto"></div>
+          <Image
+            src={"/me_2.jpeg"}
+            width={160}
+            height={160}
+            alt={"Vladyslav Kovalchuk"}
+          />
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        <pre dangerouslySetInnerHTML={{ __html: text }}></pre>
+      </div>
+      <div className={styles.cursor}></div>
+    </main>
   );
 }
